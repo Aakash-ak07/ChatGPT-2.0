76e7644088779bee1f08d2cc677147eaaf024ab6
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-02-09 23:14:07 +0530

    Update App.jsx

--- gpt/src/App.jsx
+++ gpt/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   const fetchBotResponse = async () => {
     const { data } = await axios.post(
-      "https://chatgpt-ai-app-od21.onrender.com",
+      "https://chatgpt-jk6a.onrender.com",
       { input },
       {
         headers: {
